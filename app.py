5cf6b76b1bb97c06d9ca6ea8fe13823eb437f07f
nithya <tmnithya9812gmail.com>
2026-01-29 10:14:37 +0530

    Add Render deployment configuration

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def translate_batch():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
 
 
 if __name__ == '__main__':
